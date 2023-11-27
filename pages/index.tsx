d5871cbc651d2273322606ec84dc10c0f7af4cff
Goo Nine <galen@jito.wtf>
2023-11-27 14:06:16 -0800

    add jito dao

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,15 +3,14 @@ import { useRouter } from 'next/router'
 
 const Index = () => {
   const router = useRouter()
-  const REALM = process?.env?.REALM
+  // const REALM = process?.env?.REALM
+  const REALM = 'jjCAwuuNpJCNMLAanpwgJZ6cdXzLPXe2GfD6TaDQBXt'
 
   useEffect(() => {
-    // Redirect to the jito page
     const mainUrl = REALM ? `/dao/${REALM}` : '/realms'
     if (!router.asPath.includes(mainUrl)) {
       router.replace(mainUrl)
     }
-    // This doesn't need to be here
   }, [REALM])
 
   return null
