2a9105447834e4307e8ba9b55db27eab0b2bf8a2
Goo Nine <galen@jito.wtf>
2024-01-08 12:41:05 -0800

    remove dep

--- pages/api/governance-notifier.ts
+++ pages/api/governance-notifier.ts
@@ -1,8 +1,9 @@
-import type { VercelRequest, VercelResponse } from '@vercel/node'
 import { runNotifier } from '../../scripts/governance-notifier'
 
-export default async function (req: VercelRequest, res: VercelResponse) {
-  if (req.headers.authorization !== `Bearer ${process.env.CRON_SECRET}`) {
+export default async function (req, res) {
+  console.log('req?.headers?.authorization')
+  console.log(req?.headers?.authorization)
+  if (req?.headers?.authorization !== `Bearer ${process.env.CRON_SECRET}`) {
     return res.status(401).send('Unauthorized')
   }
 
